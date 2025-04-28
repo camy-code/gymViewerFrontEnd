6942f90608c86082306d1e6008dd2fa9a4965028
camden.warburton <camden.warburton@ucalgary.ca>
2025-04-27 23:48:59 -0600

    Cam go to bed

--- src/components/GymCard.js
+++ src/components/GymCard.js
@@ -1,18 +1,19 @@
+// The following is my card imports
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material';
 
 const GymCard = ()=> {
-return <Card sx={{width:600, boxShadow:3}}>
+return <Card sx={{width:600, boxShadow:1}}>
+    <CardActionArea>
     <CardContent>
       <h2>Monday</h2>
       <p>Time: 5:00 PM - 7:00 PM</p>
       <p>Location: Gym A</p>
       <p>Sport: Basketball</p>
     </CardContent>
-    <CardActions>
-      <button>Sign Up</button>
-    </CardActions>
+    </CardActionArea>
 </Card>
 }
 export default GymCard;
