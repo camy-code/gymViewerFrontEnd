5f3350697f9df30d6dd8319e6452d597b427335e
camden.warburton <camden.warburton@ucalgary.ca>
2025-05-21 17:32:43 -0600

    Made buttons better

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -2,29 +2,35 @@
 import { Button, Box } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import ButtonDesign from "../tools/ButtonDesign";
-import { margin } from "@mui/system";
+import { height, margin, width } from "@mui/system";
 
 export function ButtonGroup({ expandClick, collapseClick }) {
-  const style123 = [{scale:0.8, marginRight:-1},
+  const style123 = [{height:50, width:150},
     ButtonDesign.getButtonStyle("purple", "blue", "white"),
   ];
 
   return (
     <>
-      <Box width={"auto"} sx={{  width:825 }}>
+      <Box width={"auto"} >
         <Grid
           container
           direction={"row"}
           
-          justifyContent={"flex-start"}
-          alignItems={"flex-start"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          spacing={1}
+          marginTop={2}
         >
+          <Box>
           <Button onClick={expandClick} sx={style123}>
             Expand
           </Button>
+          </Box>
+          <Box>
           <Button onClick={collapseClick} sx={style123}>
             Collapse
           </Button>
+          </Box>
         </Grid>
       </Box>
     </>
